4b3989581a6c46065db78f48469e41a188287cb7
wojciech.fiderek <wojciech.2.fiderek-ext@continental-corporation.com>
2017-12-04 18:28:00 +0100

    fix no 3

--- integrate.bat
+++ integrate.bat
@@ -1,13 +1,14 @@
 @echo off
 set TMP_BUILD_BRANCH=TMP_BUILD_BRANCH
 
-
 if "%~1"=="" goto NO_BASE_BRACH
 if "%~2"=="" goto NO_TARGET_BRACH
 
-
 set BASE_BRANCH=%1
 set TARGET_BRANCH=%2
+
+
+printf "\n---- Prepare -----\n"
 
 git checkout master
 
@@ -17,13 +18,22 @@ git fetch origin %TARGET_BRANCH%:%TARGET_BRANCH%
 
 git checkout -b %TMP_BUILD_BRANCH%
 git merge %BASE_BRANCH%
+
+
+printf "\n----- MERGE ------\n"
 git merge %TARGET_BRANCH%
 
+
+
+printf "\n----- BUILD ------\n"
 make all
 
+
+printf "\n------ RUN -------\n"
 main
 
 
+printf "\n----- Clean ------\n"
 git checkout master
 git branch -D %TMP_BUILD_BRANCH%
 
