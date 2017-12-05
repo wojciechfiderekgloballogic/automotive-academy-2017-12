a206db02109e78b735dee4462aa42273928b3743
Grzegorz Makowski <grzegorz.makowski@synapse.com>
2017-12-05 15:25:20 +0100

    Ended task1 with passed TEST1

---
+++ app/GRZMAK/GRZMAK_task1.c
@@ -0,0 +1,31 @@
+/**
+* Responsible: Grzegorz Makowski
+*
+*
+*History Log:
+* 2017.12.05 GRZMAK initial version
+*
+**/
+
+#ifndef GRZMAK_TASK1_C
+#define GRZMAK_TASK1_C
+
+#include "GRZMAK_task1.h"
+
+eErr_t GRZMAK_setBit(int bit, int* reg)
+{
+	return E_NOT_IMPLEMENTED;
+}
+
+eErr_t GRZMAK_clearBit(int bit, int* reg)
+{
+	return E_NOT_IMPLEMENTED;
+}
+
+eErr_t GRZMAK_invertBit(int bit, int* reg)
+{
+	return E_NOT_IMPLEMENTED;
+}
+
+
+#endif
