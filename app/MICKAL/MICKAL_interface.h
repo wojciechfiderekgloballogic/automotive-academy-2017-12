dea99201c5302c94410699eaedd3b286b1944d50
Michal Kaliszczyk <michal.kaliszczyk@synapse.com>
2017-12-05 11:59:25 +0100

    exercie

---
+++ app/MICKAL/MICKAL_interface.h
@@ -0,0 +1,10 @@
+ Volume in drive C has no label.
+ Volume Serial Number is AE41-5139
+
+ Directory of C:\Projects\automotive-academy-2017-12\app\MICKAL
+
+12/05/2017  11:57 AM    <DIR>          .
+12/05/2017  11:57 AM    <DIR>          ..
+12/05/2017  11:57 AM                 0 MICKAL_interface.h
+               1 File(s)              0 bytes
+               2 Dir(s)  231,537,631,232 bytes free
