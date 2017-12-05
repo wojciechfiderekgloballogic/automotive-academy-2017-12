3df882a91b5ad683cc5ecba062b8a8bf66b0d862
wojciech.fiderek <wojciech.2.fiderek-ext@continental-corporation.com>
2017-12-05 19:17:10 +0100

    Taks 2 tests + small modifications

--- test/tests.c
+++ test/tests.c
@@ -3,6 +3,7 @@
  *
  *
  * History log:
+ * 2017.12.05   WojFid  Task 2 tests introduced (set lamp bit)
  * 2017.12.05   TomWes  Added another interface
  * 2017.12.01   WojFid  Initial version
  */
@@ -12,6 +13,7 @@
 #include "../app/tomwes/TOMWES_interface.h"
 #include "tests.h"
 #include "test_1_setbitinterface.h"
+#include "test_2_setlampbit.h"
 
 
 int main() {
@@ -23,6 +25,24 @@ int main() {
     /* Below example should work in case if first task implemented due to requirements: */
     TEST_1__SET_BIT_INTERFACE(WOJFID);
     TEST_1__SET_BIT_INTERFACE(TOMWES);
+    //TEST_1__SET_BIT_INTERFACE(GRZMAK);
+    //TEST_1__SET_BIT_INTERFACE(MARTOR);
+    //TEST_1__SET_BIT_INTERFACE(MICKAL);
+    //TEST_1__SET_BIT_INTERFACE(PAWAZI);
+    //TEST_1__SET_BIT_INTERFACE(PIOCIE);
+    //TEST_1__SET_BIT_INTERFACE(RAFAND);
+    //TEST_1__SET_BIT_INTERFACE(JARKUR);
+    //TEST_1__SET_BIT_INTERFACE(ADAKOR);
+
+    //TEST_2__SET_LAMP_BIT(GRZMAK);
+    //TEST_2__SET_LAMP_BIT(MARTOR);
+    //TEST_2__SET_LAMP_BIT(MICKAL);
+    //TEST_2__SET_LAMP_BIT(PAWAZI);
+    //TEST_2__SET_LAMP_BIT(PIOCIE);
+    //TEST_2__SET_LAMP_BIT(RAFAND);
+    //TEST_2__SET_LAMP_BIT(TOMWES);
+    //TEST_2__SET_LAMP_BIT(JARKUR);
+    //TEST_2__SET_LAMP_BIT(ADAKOR);
     
 
     OUT_WHITE();
