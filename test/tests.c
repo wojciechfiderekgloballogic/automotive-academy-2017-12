6924c8f6279898b0a81b9368772f26f1a402bf84
Pawel Aziukiewicz <pawel.aziukiewicz@synapse.com>
2017-12-05 13:41:14 +0100

    task_1_compiled

--- test/tests.c
+++ test/tests.c
@@ -8,6 +8,7 @@
 #include <stdio.h>
 #include "../app/common/COMMON_interface.h"
 #include "../app/wojfid/WOJFID_interface.h"
+#include "../app/PAWAZI/PAWAZI_task_1.h"
 #include "tests.h"
 #include "test_1_setbitinterface.h"
 
@@ -20,6 +21,7 @@ int main() {
     /* Add tests for your module when module interface ready */
     /* Below example should work in case if first task implemented due to requirements: */
     TEST_1__SET_BIT_INTERFACE(WOJFID);
+    TEST_1__SET_BIT_INTERFACE(PAWAZI);
     
 
     OUT_WHITE();
