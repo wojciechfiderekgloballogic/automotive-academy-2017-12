32a230395f2d277b9b90a54997cbde1aeecef8db
Paw Azi <>
2017-12-06 13:41:30 +0100

    fixed interface

--- app/PAWAZI/PAWAZI_interface.c
+++ app/PAWAZI/PAWAZI_interface.c
@@ -10,19 +10,37 @@
 
 eErr_t PAWAZI_setLampBit(eLamp_t bit, int* reg){
 	
+	if(bit<0&&bit>sizeof(int)*8){
+		return E_INVALID_ARG_1;
+	}
+	
 	*reg |= 1<<bit;
+	
+	return E_OK;
 	
 }
 
 eErr_t PAWAZI_clearLampBit(eLamp_t bit, int* reg){
 	
+	if(bit<0&&sizeof(int)*8){
+		return E_INVALID_ARG_1;
+	}
+	
 	*reg &= ~(1<<bit);
+	
+	return E_OK;
 	
 }
 
 eErr_t PAWAZI_invertLampBit(eLamp_t bit, int* reg){
 	
+	if(bit<0&&bit>sizeof(int)*8){
+		return E_INVALID_ARG_1;
+	}
+	
 	*reg ^= 1<<bit;
+	
+	return E_OK;
 	
 }
 
