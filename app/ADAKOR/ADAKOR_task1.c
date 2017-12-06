4d0edeae9287ede7796857f0336cb243a7775069
Adam Kordykiewicz <adam.kordykiewicz@synapse.com>
2017-12-06 14:13:34 +0100

    uff

---
+++ app/ADAKOR/ADAKOR_task1.c
@@ -0,0 +1,27 @@
+/**
+ * Responsible: Wojciech Fiderek
+ *
+ *
+ * History log:
+ * 2017.12.05   ADAKOR  Initial version that fails tests
+ */
+
+#include "ADAKOR_task1.h"
+
+/* Every public function in your module should start with   "MODULENAME_"   prefix */
+/* Every private function in your module should start with  "MODULENAME__"  prefix */
+
+eErr_t ADAKOR_setBit(int bit, int* reg) {
+	*reg = (1 << bit) | *reg;
+    return E_NOT_IMPLEMENTED;
+}
+
+eErr_t ADAKOR_clearBit(int bit, int* reg) {
+	*reg &= ~(1 << bit);
+    return E_NOT_IMPLEMENTED;
+}
+
+eErr_t ADAKOR_invertBit(int bit, int* reg) {
+	*reg ^= (1 << bit);
+    return E_NOT_IMPLEMENTED;
+}
