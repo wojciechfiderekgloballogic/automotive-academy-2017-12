43216a2f0aaf5a7de8715b773d554781ebb5554f
Rafal Andrukajtis <rafal.andrukajtis@synapse.com>
2017-12-06 12:51:39 +0100

    add

---
+++ app/RAFAND/RAFAND_interface.h
@@ -0,0 +1,14 @@
+/**
+ * Responsible: Rafal Andrukajtis
+ *
+ *
+ * History log:
+ * 2017.12.06   RafAnd  My version
+ */
+
+#ifndef RAFAND_TASK_1_H
+#define RAFAND_TASK_1_H
+// Every function that is exposed should be placed here
+#include "RAFAND_task_1.h"
+
+#endif
