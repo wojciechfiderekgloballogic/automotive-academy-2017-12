a9a4f7faaf29dc92e7fd3a3dcc00e68d3b50e54f
Paw Azi <>
2017-12-06 11:52:54 +0100

    fixed tests

--- test/tests.c
+++ test/tests.c
@@ -3,14 +3,17 @@
  *
  *
  * History log:
+ * 2017.12.05   WojFid  Task 2 tests introduced (set lamp bit)
+ * 2017.12.05   TomWes  Added another interface
  * 2017.12.01   WojFid  Initial version
  */
 #include <stdio.h>
 #include "../app/common/COMMON_interface.h"
 #include "../app/wojfid/WOJFID_interface.h"
-#include "../app/PAWAZI/PAWAZI_task_1.h"
+#include "../app/tomwes/TOMWES_interface.h"
 #include "tests.h"
 #include "test_1_setbitinterface.h"
+#include "test_2_setlampbit.h"
 
 
 int main() {
@@ -20,8 +23,26 @@ int main() {
     
     /* Add tests for your module when module interface ready */
     /* Below example should work in case if first task implemented due to requirements: */
-    //TEST_1__SET_BIT_INTERFACE(WOJFID);
-    TEST_1__SET_BIT_INTERFACE(PAWAZI);
+    TEST_1__SET_BIT_INTERFACE(WOJFID);
+    TEST_1__SET_BIT_INTERFACE(TOMWES);
+    //TEST_1__SET_BIT_INTERFACE(GRZMAK);
+    //TEST_1__SET_BIT_INTERFACE(MARTOR);
+    //TEST_1__SET_BIT_INTERFACE(MICKAL);
+    //TEST_1__SET_BIT_INTERFACE(PAWAZI);
+    //TEST_1__SET_BIT_INTERFACE(PIOCIE);
+    //TEST_1__SET_BIT_INTERFACE(RAFAND);
+    //TEST_1__SET_BIT_INTERFACE(JARKUR);
+    //TEST_1__SET_BIT_INTERFACE(ADAKOR);
+
+    //TEST_2__SET_LAMP_BIT(GRZMAK);
+    //TEST_2__SET_LAMP_BIT(MARTOR);
+    //TEST_2__SET_LAMP_BIT(MICKAL);
+    //TEST_2__SET_LAMP_BIT(PAWAZI);
+    //TEST_2__SET_LAMP_BIT(PIOCIE);
+    //TEST_2__SET_LAMP_BIT(RAFAND);
+    //TEST_2__SET_LAMP_BIT(TOMWES);
+    //TEST_2__SET_LAMP_BIT(JARKUR);
+    //TEST_2__SET_LAMP_BIT(ADAKOR);
     
 
     OUT_WHITE();
