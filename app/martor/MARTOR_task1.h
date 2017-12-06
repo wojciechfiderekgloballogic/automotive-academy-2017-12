c346b4fc61028d9597e2bde903998bad72e8a070
TheGrayJacket <thegrayjacket@gmail.com>
2017-12-06 11:51:29 +0100

    MARTOR Task 1

---
+++ app/martor/MARTOR_task1.h
@@ -0,0 +1,18 @@
+/**
+ * Responsible: Marcin Torchala
+ *
+ *
+ * History log:
+ * 2017.12.06	MarTor	Initial Version
+ */
+#pragma once
+
+#include "../common/COMMON_interface.h"
+
+/* every public function in your module should start with "MODULENAME_" prefix */
+
+eErr_t MARTOR_setBit(int bit, int* reg);
+
+eErr_t MARTOR_clearBit(int bit, int* reg);
+
+eErr_t MARTOR_invertBit(int bit, int* reg);
