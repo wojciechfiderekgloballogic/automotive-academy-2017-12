81187aa9c43898505975501a9d9893ed82f79181
Paw Azi <>
2017-12-06 16:14:30 +0100

    compiling ok but bug in test in file test_setlampbit.c...

--- app/PAWAZI/PAWAZI_interface.h
+++ app/PAWAZI/PAWAZI_interface.h
@@ -3,20 +3,12 @@
  *
  *
  * History log:
+ * 2017.12.06 changed files structure
  */
+
 #ifndef __PAWAZI_INTERFACE__
 #define __PAWAZI_INTERFACE__
 
-#include "../common/COMMON_interface.h"
-
-eErr_t PAWAZI_setLampBit(eLamp_t bit, int* reg);
-
-eErr_t PAWAZI_clearLampBit(eLamp_t bit, int* reg);
-
-eErr_t PAWAZI_invertLampBit(eLamp_t bit, int* reg);
-
-eErr_t PAWAZI_getLampState(eLamp_t bit, int reg, eLampState_t* state);
+#include "PAWAZI_task_2.h"
 
 #endif
-
-
