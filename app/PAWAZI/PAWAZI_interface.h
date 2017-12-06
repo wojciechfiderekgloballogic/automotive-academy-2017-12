a949f3cf3f091ee30b0cc7541d872bd94ac101c2
Paw Azi <>
2017-12-06 12:27:15 +0100

    renamed interface file

--- app/PAWAZI/PAWAZI_interface.h
+++ app/PAWAZI/PAWAZI_interface.h
@@ -1,22 +1,20 @@
 /**
- * Responsible: Pawel Aziukiewicz
+ * Responsible: Pawe³ Aziukiewicz
  *
  *
  * History log:
+ * 2017.12.01   Pawe³ Aziukiewicz  Initial version
  */
 #ifndef __PAWAZI_INTERFACE__
 #define __PAWAZI_INTERFACE__
 
 #include "../common/COMMON_interface.h"
 
-eErr_t PAWAZI_setLampBit(eLamp_t bit, int* reg);
+eErr_t PAWAZI_setBit(int bit, int* reg);
 
-eErr_t PAWAZI_clearLampBit(eLamp_t bit, int* reg);
+eErr_t PAWAZI_clearBit(int bit, int* reg);
 
-eErr_t PAWAZI_invertLampBit(eLamp_t bit, int* reg);
-
-eErr_t PAWAZI_getLampState(eLamp_t bit, int reg, eLampState_t* state);
+eErr_t PAWAZI_invertBit(int bit, int* reg);
 
 #endif
-
 
