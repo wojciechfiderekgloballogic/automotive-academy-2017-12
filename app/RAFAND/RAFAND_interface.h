7c77b34d317a3727030008efc90645746d158040
Rafal Andrukajtis <rafal.andrukajtis@synapse.com>
2017-12-06 13:43:49 +0100

    aaa

--- app/RAFAND/RAFAND_interface.h
+++ app/RAFAND/RAFAND_interface.h
@@ -6,8 +6,8 @@
  * 2017.12.06   RafAnd  My version
  */
 
-#ifndef RAFAND_TASK_1_H
-#define RAFAND_TASK_1_H
+#ifndef RAFAND_INTERFACE_H
+#define RAFAND_INTERFACE_H
 // Every function that is exposed should be placed here
 #include "RAFAND_task_1.h"
 
