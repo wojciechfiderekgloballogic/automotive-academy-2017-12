58b4aaa9f48841935b4a45f13fc23ab085eb172f
Rafal Andrukajtis <rafal.andrukajtis@synapse.com>
2017-12-06 08:20:31 +0100

    RAFAND

---
+++ app/rafand/RAFAND_task_1.c
@@ -0,0 +1,26 @@
+/**
+ * Responsible: Rafal Andrukajtis
+ *
+ *
+ * History log:
+ * 2017.12.05   RafAnd  My version
+ */
+
+#include <stdio.h>
+#include "RAFAND_task_1.h"
+
+/* Every public function in your module should start with   "MODULENAME_"   prefix */
+/* Every private function in your module should start with  "MODULENAME__"  prefix */
+
+eErr_t RAFAND_setBit(int bit, int* reg) {
+    return E_NOT_IMPLEMENTED;
+}
+
+eErr_t RAFAND_clearBit(int bit, int* reg) {
+    return E_NOT_IMPLEMENTED;
+}
+
+eErr_t RAFAND_invertBit(int bit, int* reg) {
+    return E_NOT_IMPLEMENTED;
+}
+    
