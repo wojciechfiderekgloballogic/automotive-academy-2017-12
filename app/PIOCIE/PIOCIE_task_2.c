c05c65a44b2d83d0ac45cc441ef38238ac5d0d05
Piotr Ciesielczyk <piotr.ciesielczyk@globallogic.com>
2017-12-07 13:06:14 +0100

    Upgraded return

--- app/PIOCIE/PIOCIE_task_2.c
+++ app/PIOCIE/PIOCIE_task_2.c
@@ -14,48 +14,80 @@
 
 eErr_t PIOCIE_setLampBit( eLamp_t bit, int* reg)
 {
+	eErr_t result;
 	if(bit<LAMP_START||bit>=LAMP_COUNT)
 	{
-		return E_INVALID_ARG_1;
+		result=E_INVALID_ARG_1;
 	}
-	if(NULL==reg)
+	else if(NULL==reg)
 	{
-		return E_INVALID_ARG_2;
+		result=E_INVALID_ARG_2;
 	}
-	*reg|=(0x01<<bit);
-	return E_OK;
+	else
+	{
+		*reg|=(0x01<<bit);
+		result=E_OK;
+	}
+	return result;
 };
 eErr_t PIOCIE_clearLampBit(     eLamp_t bit, int* reg    )
 {
+	eErr_t result;
 	if(bit<LAMP_START||bit>=LAMP_COUNT)
-		return E_INVALID_ARG_1;
-	if(NULL==reg)
-		return E_INVALID_ARG_2;
-	
-	*reg&=~(0x01<<bit);
-	return E_OK;
+	{
+		result=E_INVALID_ARG_1;
+	}
+	else if(NULL==reg)
+	{
+		result=E_INVALID_ARG_2;
+	}
+	else
+	{
+		*reg&=~(0x01<<bit);
+		result=E_OK;
+	}
+	return result;
 };
 eErr_t PIOCIE_invertLampBit(    eLamp_t bit, int* reg    )
 {
+	eErr_t result;
 	if(bit<LAMP_START||bit>=LAMP_COUNT)
-		return E_INVALID_ARG_1;
-	if(NULL==reg)
-		return E_INVALID_ARG_2;
-	
-	*reg^=(0x01<<bit);
-	
-	return E_OK;
+	{
+		result=E_INVALID_ARG_1;
+	}
+	else if(NULL==reg)
+	{
+		result=E_INVALID_ARG_2;
+	}
+	else
+	{
+		*reg^=(0x01<<bit);
+		result=E_OK;
+	}
+	return result;
 };
 eErr_t PIOCIE_getLampState(       eLamp_t bit, int reg, eLampState_t* state)
 {
+	eErr_t result;
 	if(bit<LAMP_START||bit>=LAMP_COUNT)
-		return E_INVALID_ARG_1;
-	if(NULL==state)
-		return E_INVALID_ARG_2;
-	
-	if(((reg)&(0x01<<bit)))
-		*state=LAMP_STATE_ON;
+	{
+		result=E_INVALID_ARG_1;
+	}
+	else if(NULL==state)
+	{
+		result=E_INVALID_ARG_2;
+	}
 	else
-		*state=LAMP_STATE_OFF;
-	return E_OK;
+	{
+		if(((reg)&(0x01<<bit)))
+		{
+			*state=LAMP_STATE_ON;
+		}
+		else
+		{
+			*state=LAMP_STATE_OFF;
+		}
+		result=E_OK;
+	}
+	return result;
 };
