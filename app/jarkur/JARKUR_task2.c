e81b3d080df408d410d88aec184e5c1807fd9a19
Letero <jaroslaw.kurzynski@synapse.com>
2017-12-07 09:46:44 +0100

    task_2

--- app/jarkur/JARKUR_task2.c
+++ app/jarkur/JARKUR_task2.c
@@ -10,13 +10,19 @@
 #include "JARKUR_task2.h"
 
 eErr_t JARKUR_setLampBit(eLamp_t bit, int* reg) {
-    int mask = 1;
+	if (bit < 0 || bit > 18) {
+		return E_INVALID_ARG_1;
+	}
+	int mask = 1;
     mask = mask << bit;
     *reg |= mask;
     return E_OK;
 }
 
 eErr_t JARKUR_clearLampBit(eLamp_t bit, int* reg) {
+	if (bit < 0 || bit > 18) {
+		return E_INVALID_ARG_1;
+	}
     int mask = 1;
 	mask = mask << bit;
 	mask = ~mask;
@@ -25,6 +31,9 @@ eErr_t JARKUR_clearLampBit(eLamp_t bit, int* reg) {
 }
 
 eErr_t JARKUR_invertLampBit(eLamp_t bit, int* reg) {
+	if (bit < 0 || bit > 18) {
+		return E_INVALID_ARG_1;
+	}
     int mask = 1;
 	mask = mask << bit;
 	*reg ^= mask;
@@ -32,5 +41,15 @@ eErr_t JARKUR_invertLampBit(eLamp_t bit, int* reg) {
 }
 
 eErr_t JARKUR_getLampState( eLamp_t bit, int reg, eLampState_t* state) {
+	if (bit < 0 || bit > 18) {
+		return E_INVALID_ARG_1;
+	}
+	int mask = 1;
+	mask = mask << bit;
+	if (reg & mask) {
+		*state = LAMP_STATE_ON;
+	} else {
+		*state = LAMP_STATE_OFF;
+	}
     return E_OK;
 }
