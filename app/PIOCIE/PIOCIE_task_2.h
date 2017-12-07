a9bf428afb534e62dc8cf0cf5f74334f9ceedc4b
Piotr Ciesielczyk <piotr.ciesielczyk@globallogic.com>
2017-12-07 15:31:28 +0100

    Interface 3 implemented

---
+++ app/PIOCIE/PIOCIE_task_2.h
@@ -0,0 +1,21 @@
+/**
+ * Responsible: Piotr Ciesielczyk
+ *
+ *
+ * History log:
+ * 2017.12.05   PioCie  Initial version
+ */
+#ifndef PIOCIE_TASK_2_H
+#define PIOCIE_TASK_2_H
+
+
+#include "../common/COMMON_interface.h"
+
+/* every public function in your module should start with "MODULENAME_" prefix */
+
+eErr_t PIOCIE_setLampBit(eLamp_t bit, int* reg);
+eErr_t PIOCIE_clearLampBit(eLamp_t bit, int* reg);
+eErr_t PIOCIE_invertLampBit(eLamp_t bit, int* reg);
+eErr_t PIOCIE_getLampState(eLamp_t bit, int reg, eLampState_t* state);
+
+#endif
