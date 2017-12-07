cd65adcafa8ef46e26a659863361237eec8a2145
Paw Azi <>
2017-12-07 09:32:17 +0100

    added removed task_0

---
+++ app/PAWAZI/PAWAZI_task_1.c
@@ -0,0 +1,25 @@
+/**
+ * Responsible: Pawe³ Aziukiewicz
+ *
+ * History log:
+ * 
+ */
+
+#include <stdio.h>
+#include "PAWAZI_task_1.h"
+
+/* Every public function in your module should start with   "MODULENAME_"   prefix */
+/* Every private function in your module should start with  "MODULENAME__"  prefix */
+
+eErr_t PAWAZI_setBit(int bit, int* reg) {
+    return E_NOT_IMPLEMENTED;
+}
+
+eErr_t PAWAZI_clearBit(int bit, int* reg) {
+     return E_NOT_IMPLEMENTED;
+}
+
+eErr_t PAWAZI_invertBit(int bit, int* reg) {
+    return E_NOT_IMPLEMENTED;
+}
+    
