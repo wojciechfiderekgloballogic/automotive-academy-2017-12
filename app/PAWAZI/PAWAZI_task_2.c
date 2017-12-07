d9347cac4ed95977328d4ba0a563733ab6647001
Paw Azi <>
2017-12-07 13:31:43 +0100

    one return only

--- app/PAWAZI/PAWAZI_task_2.c
+++ app/PAWAZI/PAWAZI_task_2.c
@@ -9,65 +9,73 @@
 #include <stdio.h>
 #include "PAWAZI_task_2.h"
 
+static const enum eErr resErr[] = { E_OK, E_INVALID_ARG_1, E_INVALID_ARG_2, E_NOT_IMPLEMENTED, E_MALLOC };
+
 eErr_t PAWAZI_setLampBit(eLamp_t bit, int* reg){
+
+	int res = 0;
 	
 	if(bit<LAMP_START || bit>=LAMP_COUNT){
-		return E_INVALID_ARG_1;
+		res = 1;
 	}else if(reg == NULL){
-		return E_INVALID_ARG_2;
+		res = 2;
+	}else{
+		*reg |= 1<<bit;
 	}
 	
-	
-	*reg |= 1<<bit;
-	
-	return E_OK;
+	return resErr[res];
 	
 	
 }
 
 eErr_t PAWAZI_clearLampBit(eLamp_t bit, int* reg){
 	
-	
+	int res = 0;
 	
 	if(bit<LAMP_START || bit>=LAMP_COUNT){
-		return E_INVALID_ARG_1;
+		res = 1;
 	}else if(reg == NULL){
-		return E_INVALID_ARG_2;
+		res = 2;
+	}else{
+		*reg &= ~(1<<bit);
 	}
 	
-	*reg &= ~(1<<bit);
-	
-	return E_OK;
+	return resErr[res];
 	
 }
 
 eErr_t PAWAZI_invertLampBit(eLamp_t bit, int* reg){
 	
 	
+	int res = 0;
+	
 	if(bit<LAMP_START || bit>=LAMP_COUNT){
-		return E_INVALID_ARG_1;
+		res = 1;
 	}else if(reg == NULL){
-		return E_INVALID_ARG_2;
+		res = 2;
+	}else{
+		*reg ^= 1<<bit;
 	}
 	
-	*reg ^= 1<<bit;
-	
-	return E_OK;
+	return resErr[res];
 	
 }
 
 eErr_t PAWAZI_getLampState(eLamp_t bit, int reg, eLampState_t* state){
 	
-	if(state == NULL){
-		return E_INVALID_ARG_2;
-	}else if(bit<LAMP_START || bit>=LAMP_COUNT){
-		return E_INVALID_ARG_1;
-	} 
+	int res = 0;
 	
+	if(bit<LAMP_START || bit>=LAMP_COUNT){
+		res = 1;
+	}else if(state == NULL){
+		res = 2;
+	}
+		
 	if(((reg >> bit) & 0x01)){
 		*state = LAMP_STATE_ON;
 	}else *state = LAMP_STATE_OFF;	
+
 		
-	return E_OK;
+	return resErr[res];
 }
     
