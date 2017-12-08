1515fb011e6c8232aaaada6de747827669923b54
Piotr Ciesielczyk <piotr.ciesielczyk@globallogic.com>
2017-12-08 07:44:00 +0100

    Removed printfs; upgraded variable names

--- app/PIOCIE/PIOCIE_task_3.c
+++ app/PIOCIE/PIOCIE_task_3.c
@@ -12,74 +12,70 @@
 /* Every public function in your module should start with   "MODULENAME_"   prefix */
 /* Every private function in your module should start with  "MODULENAME__"  prefix */
 
-static uint8_t g_u8rgStorage[LAMP_COUNT%8?LAMP_COUNT/8+1:LAMP_COUNT/8];
+static uint8_t au8Storage[LAMP_COUNT%8?LAMP_COUNT/8+1:LAMP_COUNT/8];
 
 eErr_t PIOCIE_lampOn(eLamp_t eLamp)
 {
-	eErr_t result;
+	eErr_t eResult;
 	if(eLamp<LAMP_START||eLamp>=LAMP_COUNT)
 	{
-		result=E_INVALID_ARG_1;
+		eResult=E_INVALID_ARG_1;
 	}
-	else if(NULL==g_u8rgStorage)
+	else if(NULL==au8Storage)
 	{
-		result=E_INVALID_ARG_2;
+		eResult=E_INVALID_ARG_2;
 	}
 	else
 	{
-		g_u8rgStorage[eLamp/8]|=(0x01<<(eLamp%8));
-		printf("\nset ON %d:%d",eLamp,g_u8rgStorage[eLamp/8]);
-		result=E_OK;
+		au8Storage[eLamp/8]|=(0x01<<(eLamp%8));
+		eResult=E_OK;
 	}
-	return result;
+	return eResult;
 };
 
 eErr_t PIOCIE_lampOff(eLamp_t eLamp)
 {
-	eErr_t result;
+	eErr_t eResult;
 	if(eLamp<LAMP_START||eLamp>=LAMP_COUNT)
 	{
-		result=E_INVALID_ARG_1;
+		eResult=E_INVALID_ARG_1;
 	}
-	else if(NULL==g_u8rgStorage)
+	else if(NULL==au8Storage)
 	{
-		result=E_INVALID_ARG_2;
+		eResult=E_INVALID_ARG_2;
 	}
 	else
 	{
-		g_u8rgStorage[eLamp/8]&=~(0x01<<(eLamp%8));
-		printf("\nset OFF %d:%d",eLamp,g_u8rgStorage[eLamp/8]);
-		result=E_OK;
+		au8Storage[eLamp/8]&=~(0x01<<(eLamp%8));
+		eResult=E_OK;
 	}
-	return result;
+	return eResult;
 };
 
 eErr_t PIOCIE_getLamp(eLamp_t eLamp, eLampState_t* eState)
 {
-	eErr_t result;
+	eErr_t eResult;
 	if(eLamp<LAMP_START||eLamp>=LAMP_COUNT)
 	{
-		result=E_INVALID_ARG_1;
+		eResult=E_INVALID_ARG_1;
 		*eState=LAMP_STATE_UNDEFINED;
 	}
 	else if(NULL==eState)
 	{
-		result=E_INVALID_ARG_2;
+		eResult=E_INVALID_ARG_2;
 		*eState=LAMP_STATE_UNDEFINED;
 	}
 	else
 	{
-		if(((g_u8rgStorage[eLamp/8])&(0x01<<(eLamp%8))))
+		if(((au8Storage[eLamp/8])&(0x01<<(eLamp%8))))
 		{
-			printf("\n2:");
 			*eState=LAMP_STATE_ON;
 		}
 		else
 		{
-			printf("\n1:");
 			*eState=LAMP_STATE_OFF;
 		}
-		result=E_OK;
+		eResult=E_OK;
 	}
-	return result;
+	return eResult;
 };
