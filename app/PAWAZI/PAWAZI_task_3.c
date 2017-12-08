ed39c48a0d29f84d4c311e687e45574a2ce16c28
Paw Azi <>
2017-12-08 14:17:07 +0100

    fixed typo

--- app/PAWAZI/PAWAZI_task_3.c
+++ app/PAWAZI/PAWAZI_task_3.c
@@ -9,7 +9,7 @@
 #include <stdio.h>
 #include "PAWAZI_task_3.h"
 
-static uint8_t _uiStorage[ (LAMP_COUNT%2) ? ((int)(LAMP_COUNT/8)+1) : (LAMP_COUNT/8) ];
+static uint8_t _ui8Storage[ (LAMP_COUNT%2) ? ((int)(LAMP_COUNT/8)+1) : (LAMP_COUNT/8) ];
 
 eErr_t PAWAZI_lampOn(eLamp_t eLamp){
 	
@@ -18,7 +18,7 @@ eErr_t PAWAZI_lampOn(eLamp_t eLamp){
 	if(eLamp<LAMP_START || eLamp>=LAMP_COUNT){
 		eRes = E_INVALID_ARG_1;
 	}else {
-		_uiStorage[eLamp/8] |= 1<<(eLamp%8);
+		_ui8Storage[eLamp/8] |= 1<<(eLamp%8);
 	}
 	
 	return eRes;
@@ -33,7 +33,7 @@ eErr_t PAWAZI_lampOff(eLamp_t eLamp){
 	if(eLamp<LAMP_START || eLamp>=LAMP_COUNT){
 		eRes = E_INVALID_ARG_1;
 	}else {
-		_uiStorage[eLamp/8] &= ~(1<<(eLamp%8));
+		_ui8Storage[eLamp/8] &= ~(1<<(eLamp%8));
 	}
 	
 	return eRes;
@@ -49,7 +49,7 @@ eErr_t PAWAZI_getLamp(eLamp_t eLamp, eLampState_t* eState){
 		eRes = E_INVALID_ARG_1;
 	}else if(eState == NULL){
 		eRes = E_INVALID_ARG_2;
-	}else if((_uiStorage[eLamp/8]>>(eLamp%8)) & 0x1){
+	}else if((_ui8Storage[eLamp/8]>>(eLamp%8)) & 0x1){
 		*eState = LAMP_STATE_ON;
 	}else{
 		*eState = LAMP_STATE_OFF;
