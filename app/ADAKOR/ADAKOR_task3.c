b88630f8d0a231ebc88d65257aeb305087630991
Adam Kordykiewicz <adam.kordykiewicz@synapse.com>
2017-12-08 08:21:05 +0100

    Finished task 3

--- app/ADAKOR/ADAKOR_task3.c
+++ app/ADAKOR/ADAKOR_task3.c
@@ -12,57 +12,64 @@
 /* Every public function in your module should start with   "MODULENAME_"   prefix */
 /* Every private function in your module should start with  "MODULENAME__"  prefix */
 
-static uint8_t rgu8Arr [(LAMP_COUNT % 8) ? LAMP_COUNT / 8 + 1 : LAMP_COUNT / 8];
+static uint8_t arru8Storage [(LAMP_COUNT % 8) ? LAMP_COUNT / 8 + 1 : LAMP_COUNT / 8];
 
 eErr_t ADAKOR_lampOn(eLamp_t eLamp)
 {
-	eErr_t result;
+	eErr_t eResult;
 	if (eLamp >= LAMP_START && eLamp <= LAMP_COUNT)
 	{
-		rgu8Arr[eLamp / 8] = (1 << eLamp % 8) | rgu8Arr[eLamp / 8];
-		result = E_OK;
+		arru8Storage[eLamp / 8] |= (1 << eLamp % 8);
+		eResult = E_OK;
 	}
 	else
 	{
-		result = E_INVALID_ARG_1;
+		eResult = E_INVALID_ARG_1;
 	}
-	return result;
+	return eResult;
 }
 
 eErr_t ADAKOR_lampOff(eLamp_t eLamp)
 {
-	eErr_t result;
+	eErr_t eResult;
 	if (eLamp >= LAMP_START && eLamp <= LAMP_COUNT)
 	{
-		rgu8Arr[eLamp / 8] &= ~(1 << eLamp % 8);
-		result = E_OK;
+		arru8Storage[eLamp / 8] &= ~(1 << eLamp % 8);
+		eResult = E_OK;
 	}
 	else
 	{
-		result = E_INVALID_ARG_1;
+		eResult = E_INVALID_ARG_1;
 	}
-	return result;
+	return eResult;
 }
 
 eErr_t ADAKOR_getLamp(eLamp_t eLamp, eLampState_t* eState)
 {
-	eErr_t result;
-	if (eLamp >= LAMP_START && eLamp <= LAMP_COUNT)
+	eErr_t eResult;
+	if (eState == 0)
 	{
-		if ((rgu8Arr[eLamp / 8]) & (1 << eLamp % 8))
-		{
-			*eState = LAMP_STATE_ON;
-		}
-		else
-		{
-			*eState = LAMP_STATE_OFF;
-		}
-		return E_OK;
+		eResult = E_INVALID_ARG_2;
 	}
 	else
 	{
-		*eState = LAMP_STATE_UNDEFINED;
-		result = E_INVALID_ARG_1;
+		if (eLamp >= LAMP_START && eLamp <= LAMP_COUNT)
+		{
+			if ((arru8Storage[eLamp / 8]) & (1 << eLamp % 8))
+			{
+				*eState = LAMP_STATE_ON;
+			}
+			else
+			{
+				*eState = LAMP_STATE_OFF;
+			}
+			return eResult;
+		}
+		else
+		{
+			*eState = LAMP_STATE_UNDEFINED;
+			eResult = E_INVALID_ARG_1;
+		}
 	}
-	return result;
+	return eResult;
 }
