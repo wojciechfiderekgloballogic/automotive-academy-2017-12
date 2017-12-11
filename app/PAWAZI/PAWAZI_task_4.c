e4da37c72ceef0b445cc0ad0e2ac0298c29168ae
Paw Azi <>
2017-12-11 14:19:44 +0100

    trash

---
+++ app/PAWAZI/PAWAZI_task_4.c
@@ -0,0 +1,33 @@
+/**
+ * Responsible: Paw Azi
+ *
+ *
+ * History log:
+ *  06.12.2017 initial version
+ */
+
+#include <stdio.h>
+#include "PAWAZI_task_4.h"
+#include "IO_interface.h"
+
+static  eErr_t (*onPressListener) (eButton_t) [1];
+
+void   vHandleButtons(void){
+	
+    return E_NOT_IMPLEMENTED;
+	
+	
+}
+
+eErr_t eAddOnPressListener(eButton_t eButton, eErr_t (*onPressListener) (eButton_t)){
+	
+	return E_NOT_IMPLEMENTED;
+	
+}
+
+
+eErr_t eAddOnReleaseListener(eButton_t eButton, eErr_t (*onPressListener) (eButton_t)){
+	
+	return E_NOT_IMPLEMENTED;
+	
+}
---
+++ app/PAWAZI/PAWAZI_task_4.h
@@ -0,0 +1,19 @@
+/**
+ * Responsible: Paw Azi
+ *
+ *
+ * History log:
+ */
+#ifndef __PAWAZI_TASK_4__
+#define __PAWAZI_TASK_4__
+
+#include <stdint.h>
+#include "../common/COMMON_interface.h"
+
+void   vHandleButtons        (void);
+eErr_t eAddOnPressListener   (eButton_t eButton, eErr_t (*onPressListener) (eButton_t));
+eErr_t eAddOnReleaseListener (eButton_t eButton, eErr_t (*onPressListener) (eButton_t));
+
+#endif
+
+
